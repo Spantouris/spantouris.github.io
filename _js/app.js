52a503b3178ba5c614f067f525e75f82bd26ba0e
John Spantouris <john.spantouris@gmail.com>
2022-02-01 20:08:46 +0200

    Changes in projects-timeline

--- _js/app.js
+++ _js/app.js
@@ -1,6 +1,6 @@
 setTimeout(function() {
   fadeOutPreloader(document.getElementById('preloader'), 69);
-}, 1500);
+}, 700);
 
 $(document).ready(function() {
   $(window).on('beforeunload', function() {
